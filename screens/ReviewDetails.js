24d2fb13b4da8f215ae391141a9f8e31d26a874d
Aryan <aryankumar1504@gmail.com>
2021-06-11 12:18:48 +0530

    Screens, Font Family, and global styles added

---
+++ screens/ReviewDetails.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native';
+import { globalStyles } from '../styles/global';
+
+export default function Review() {
+  return (
+    <View style={globalStyles.container}>
+      <Text>Review Screen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
